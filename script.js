5647439e12cd2a215e1f82bda558f51f04155359
israel-sahar <37867238+israel-sahar@users.noreply.github.com>
2019-08-02 00:34:52 +0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
-    $('#listCountries').append('<a class="dropdown-item" href="#">Goo</a>')
-    $('#listCountries').append('<a class="dropdown-item" href="#">Boo</a>')
+    $("#loginBtn").click(function () {
+
+        $("#LoginNav").slideToggle("slow")
+    })
 })
